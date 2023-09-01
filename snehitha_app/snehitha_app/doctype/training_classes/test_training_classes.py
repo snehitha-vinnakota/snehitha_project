66a5ea7be9eb372993429bff2ef4600c56dbaec4
sreejani-vankayala <sreejanivankayala@gmail.com>
2023-09-01 04:46:16 +0000

    doctype

---
+++ snehitha_app/snehitha_app/doctype/training_classes/test_training_classes.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Snehitha and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTrainingClasses(FrappeTestCase):
+	pass
---
+++ snehitha_app/snehitha_app/doctype/training_classes/training_classes.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Snehitha and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Training Classes', {
+	// refresh: function(frm) {
+
+	// }
+});
